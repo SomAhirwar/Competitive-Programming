73df92c21e25fac89fc6f438d4516f8829046a52
Som Ahirwar <ahirwar.som@gmail.com>
2022-01-28 23:44:34 +0530

    solving leetcode problem

---
+++ LeetCode/55. Jump Game.cpp
@@ -0,0 +1,21 @@
+//Two Pointer
+//maxVisit pointer points to the index that could be reached by jumping
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxVisit = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(i>maxVisit)	//cannot react i'th index by jumping
+                break;
+
+            maxVisit = max(maxVisit, i + nums[i]);
+            if(maxVisit >= n-1)	//Can react to last index by jumping
+                return true;
+        }
+        
+        return false;
+    }
+};
